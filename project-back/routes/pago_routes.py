59066587472da6716f1dd7cfde5098c12c5ff5b2
David <davidmarcelino80@gmail.com>
2025-02-06 16:13:38 +0100

    endpoint para obtener los pagos recurrentes

--- project-back/routes/pago_routes.py
+++ project-back/routes/pago_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from utils import token_required
 from db import db
-from Modelos import PagoRecurrente
+from Modelos import Categoria, PagoRecurrente
 from datetime import datetime
 from scheduler import procesar_pagos_recurrentes
 
@@ -25,6 +25,41 @@ def agregar_pago_recurrente(decoded):
     db.session.commit()
     
     return jsonify({"message": "Pago recurrente agregado correctamente."}), 201
+
+@pago_bp.route('/getPagosRecurrentes', methods=['GET'])
+@token_required
+def getPagosRecurrentes(decoded):
+    userId = decoded['user_id']
+
+    pagos = db.session.query(
+        PagoRecurrente.id,
+        PagoRecurrente.user_id,
+        PagoRecurrente.concepto,
+        PagoRecurrente.tipo,
+        PagoRecurrente.frecuencia,
+        PagoRecurrente.siguiente_pago,
+        PagoRecurrente.estado,
+        Categoria.nombre.label("categoria")
+    ).join(
+        Categoria, Categoria.id == PagoRecurrente.categoria_id
+    ).filter(
+        PagoRecurrente.user_id == userId
+    ).all()
+
+    pagos_data = []
+    for pago in pagos:
+        pagos_data.append({
+            'id': pago.id,
+            'user_id': pago.user_id,
+            'concepto': pago.concepto,
+            'tipo': pago.tipo,
+            'frecuencia': pago.frecuencia,
+            'siguiente_pago': pago.siguiente_pago,
+            'estado': pago.estado,
+            'categoria': pago.categoria
+        })
+    # Retornar los registros en formato JSON
+    return {'pagos': pagos_data}, 200
 
 @pago_bp.route('/forzar-pagos', methods=['GET'])
 @token_required
