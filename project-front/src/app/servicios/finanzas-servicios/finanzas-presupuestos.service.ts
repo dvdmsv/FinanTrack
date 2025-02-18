f881bba8cede7a9e27095e8d6bd7efeb957f7b0d
David <davidmarcelino80@gmail.com>
2025-02-18 16:12:46 +0100

    Implementada funcionalidad para editar los valores de un presupuesto

--- project-front/src/app/servicios/finanzas-servicios/finanzas-presupuestos.service.ts
+++ project-front/src/app/servicios/finanzas-servicios/finanzas-presupuestos.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
-import { GetPresupuestosResponse } from '../../interfaces/responses';
+import { GetPresupuestosResponse, Presupuesto } from '../../interfaces/responses';
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +14,22 @@ export class FinanzasPresupuestosService {
   private PESUPUESTO = environment.PRESUPUESTO;
 
   getPresupuestos() {
-    return this.http.get<GetPresupuestosResponse>(this.API_URL + this.PESUPUESTO + '/getPresupuesto');
+    return this.http.get<GetPresupuestosResponse>(this.API_URL + this.PESUPUESTO + '/getPresupuestos');
+  }
+
+  getPresupuesto(id: number) {
+    return this.http.post<Presupuesto>(this.API_URL + this.PESUPUESTO + '/getPresupuesto', { id });
   }
 
   setPresupuesto(categoria: string, porcentaje: number) {
-    return this.http.post(this.API_URL + this.PESUPUESTO + '/setPresupuesto',{ categoria, porcentaje });
+    return this.http.post(this.API_URL + this.PESUPUESTO + '/setPresupuesto', { categoria, porcentaje });
   }
 
   deletePresupuesto(id: number) {
     return this.http.delete(`${this.API_URL}${this.PESUPUESTO}/deletePresupuesto/${id}`);
+  }
+
+  updatePresupuesto(categoria: string, id: number, porcentaje: number) {
+    return this.http.post(this.API_URL + this.PESUPUESTO + '/updatePresupuesto', { categoria, id, porcentaje });
   }
 }
