b8a2f7c6bef33d86931aaee1afb3f090f61e322c
David <davidmarcelino80@gmail.com>
2025-02-28 11:16:39 +0100

    Cambios para que funcione en un hosting

--- project-back/config.py
+++ project-back/config.py
@@ -4,7 +4,7 @@ import secrets
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URI',
-        'mysql+pymysql://root:root@127.0.0.1:33060/finanzas'
+        'mysql+pymysql://dvdmsv:finantrack@dvdmsv.mysql.pythonanywhere-services.com/dvdmsv$finanzas'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_urlsafe(64)
--- project-front/src/environments/environment.development.ts
+++ project-front/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'http://127.0.0.1:5000',
+    API_URL: 'https://dvdmsv.pythonanywhere.com',
     AUTH: '/auth',
     CATEGORIA: '/categoria',
     PRESUPUESTO: '/presupuesto',
