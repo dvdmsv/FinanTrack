6f961ff0c714621f5c506aadefc98c26eccfa1bd
David <davidmarcelino80@gmail.com>
2025-03-10 12:48:43 +0100

    Enlace para registrarse y loguearse en login y registro

--- project-front/src/app/vistas/registro/registro.component.ts
+++ project-front/src/app/vistas/registro/registro.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../servicios/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css',
 })
