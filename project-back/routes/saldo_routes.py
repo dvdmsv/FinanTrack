44105c8a76289595c36e7f176f1e07f13c199e11
David <davidmarcelino80@gmail.com>
2025-03-19 14:28:30 +0100

    refacrtorizacion de saldo

--- project-back/routes/saldo_routes.py
+++ project-back/routes/saldo_routes.py
@@ -10,19 +10,27 @@ saldo_bp = Blueprint('saldo', __name__)
 def setSaldo(decoded):
     data = request.json
     userId = decoded['user_id']
-    user = User.query.filter_by(id=userId).first()
-    user.saldo = data.get('saldo')
+
+    user = User.query.filter_by(id=userId).first_or_404()
+
+    # Actualizar saldo con el valor validado
+    user.saldo = float(data['saldo'])
     db.session.commit()
+
     return jsonify({
-        'message': 'Saldo updated',
-        'saldo': user.saldo
+        "message": "Saldo actualizado",
+        "saldo": user.saldo
     }), 200
 
 @saldo_bp.route('/getSaldo', methods=['GET'])
 @token_required
 def getSaldo(decoded):
     userId = decoded['user_id']
-    user = User.query.filter_by(id=userId).first()
+    
+    # Obtener usuario, si no existe, devuelve 404 automáticamente
+    user = User.query.filter_by(id=userId).first_or_404()
+
     return jsonify({
-        'saldo': user.saldo
+        "saldo": float(user.saldo)  # Aseguramos que el saldo se devuelve en formato numérico
     }), 200
+
